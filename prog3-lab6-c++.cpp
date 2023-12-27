7fb33dc191a81740b7fd07d9c2c5a601107d4caa
vladimirruppel <vladimir.ruppel2004@mail.ru>
2023-12-27 15:30:35 +0700

    Добавьте файлы проекта.

---
+++ prog3-lab6-c++.cpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+#define NOMINMAX
+#include <Windows.h>
+#include "AuditionsNumber.h"
+#include "AudioFile.h"
+#include "Playlist.h"
+#include "AudioPlayer.h"
+#include "AudioCollection.h"
+#include "MusicLibrary.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    
+}
